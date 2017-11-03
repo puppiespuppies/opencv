4c0fb1295cfae77bf1cdbad0fdd51a1c501526ca
Ethan <Ereish02@gmail.com>
2017-11-03 08:55:00 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,17 @@
+import numpy as np
 import cv2
-import sys
-
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(classifier/cascade.xml)
-
-video_capture = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('cascade.xml')
 
 while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=4,
-        minSize=(22, 22),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    cv2.imshow('img',img)
+    cv2.waitKey(1)
+cam.close()
 cv2.destroyAllWindows()
